2684b16c5e44054869fde61e203b9994bc284efe
Maxence LE CLERC <shadange@hotmail.com>
2024-08-09 12:00:29 +0200

    Intégration du header et du footer

--- wp-content/themes/oceanwp-child/functions.php
+++ wp-content/themes/oceanwp-child/functions.php
@@ -22,10 +22,3 @@ endif;
 add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
 // END ENQUEUE PARENT ACTION
-
-add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-function theme_enqueue_styles()
-{
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
-}
